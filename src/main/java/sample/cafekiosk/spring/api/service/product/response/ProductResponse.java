aab383fd65ad60075c28b9c68fa677d8aac2d0e4
bak3839 <bak3839@naver.com>
2025-06-06 17:16:02 +0900

    data.sql 테스트 데이터 구성

--- src/main/java/sample/cafekiosk/spring/api/service/product/response/ProductResponse.java
+++ src/main/java/sample/cafekiosk/spring/api/service/product/response/ProductResponse.java
@@ -1,10 +1,12 @@
 package sample.cafekiosk.spring.api.service.product.response;
 
 import lombok.Builder;
+import lombok.Getter;
 import sample.cafekiosk.spring.domain.product.Product;
 import sample.cafekiosk.spring.domain.product.ProductSellingStatus;
 import sample.cafekiosk.spring.domain.product.ProductType;
 
+@Getter
 public class ProductResponse {
     private Long id;
 
@@ -12,18 +14,18 @@ public class ProductResponse {
 
     private ProductType type;
 
-    private ProductSellingStatus sellingType;
+    private ProductSellingStatus sellingStatus;
 
     private String name;
 
     private int price;
 
     @Builder
-    private ProductResponse(Long id, String productNumber, ProductType type, ProductSellingStatus sellingType, String name, int price) {
+    private ProductResponse(Long id, String productNumber, ProductType type, ProductSellingStatus sellingStatus, String name, int price) {
         this.id = id;
         this.productNumber = productNumber;
         this.type = type;
-        this.sellingType = sellingType;
+        this.sellingStatus = sellingStatus;
         this.name = name;
         this.price = price;
     }
@@ -33,7 +35,7 @@ public class ProductResponse {
                 .id(product.getId())
                 .productNumber(product.getProductNumber())
                 .type(product.getType())
-                .sellingType(product.getSellingStatus())
+                .sellingStatus(product.getSellingStatus())
                 .name(product.getName())
                 .price(product.getPrice())
                 .build();
