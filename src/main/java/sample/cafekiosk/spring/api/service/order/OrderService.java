100cae2f04d54f4d1fc488d5863179b6ab2b92f0
bak3839 <bak3839@naver.com>
2025-06-07 15:52:55 +0900

    상품번호 리스트로 상품들을 조회하는 기능 구현

--- src/main/java/sample/cafekiosk/spring/api/service/order/OrderService.java
+++ src/main/java/sample/cafekiosk/spring/api/service/order/OrderService.java
@@ -4,12 +4,20 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import sample.cafekiosk.spring.api.controller.order.request.OrderCreateRequest;
 import sample.cafekiosk.spring.api.service.order.response.OrderResponse;
+import sample.cafekiosk.spring.domain.product.ProductRepository;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class OrderService {
+    private final ProductRepository productRepository;
 
     public OrderResponse createOrder(OrderCreateRequest request) {
+        List<String> productNumbers = request.getProductNumbers();
+        // Product
+        productRepository.
+        // Order
         return null;
     }
 }
