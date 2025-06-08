81f48f0ac7d2a8c01c063232e2979224e1c6c05d
bak3839 <bak3839@naver.com>
2025-06-08 23:53:33 +0900

    OrderCreateRequest validation 적용 테스트

--- src/main/java/sample/cafekiosk/spring/api/service/order/OrderService.java
+++ src/main/java/sample/cafekiosk/spring/api/service/order/OrderService.java
@@ -3,7 +3,7 @@ package sample.cafekiosk.spring.api.service.order;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import sample.cafekiosk.spring.api.controller.order.request.OrderCreateRequest;
+import sample.cafekiosk.spring.api.service.order.request.OrderCreateServiceRequest;
 import sample.cafekiosk.spring.api.service.order.response.OrderResponse;
 import sample.cafekiosk.spring.domain.order.Order;
 import sample.cafekiosk.spring.domain.order.OrderRepository;
@@ -32,7 +32,7 @@ public class OrderService {
      * optimistic lock / pessimistic lock / ...
      * 비관적 락 / 낙관적 락
      */
-    public OrderResponse createOrder(OrderCreateRequest request, LocalDateTime registeredDateTime) {
+    public OrderResponse createOrder(OrderCreateServiceRequest request, LocalDateTime registeredDateTime) {
         List<String> productNumbers = request.getProductNumbers();
         List<Product> products = findProductsBy(productNumbers);
 
@@ -53,7 +53,7 @@ public class OrderService {
             Stock stock = stockMap.get(stockProductNumber);
             int quantity = productCountingMap.get(stockProductNumber).intValue();
 
-            if(stock.isQuantityLessThan(quantity)) {
+            if (stock.isQuantityLessThan(quantity)) {
                 throw new IllegalArgumentException("재고가 부족한 상품이 있습니다.");
             }
 
@@ -63,29 +63,29 @@ public class OrderService {
 
     private List<String> extractStockProductNumbers(List<Product> products) {
         return products.stream()
-                .filter(product -> ProductType.containsStockType(product.getType()))
-                .map(Product::getProductNumber)
-                .collect(Collectors.toList());
+            .filter(product -> ProductType.containsStockType(product.getType()))
+            .map(Product::getProductNumber)
+            .collect(Collectors.toList());
     }
 
     private Map<String, Stock> createStockMapBy(List<String> stockProductNumbers) {
         List<Stock> stocks = stockRepository.findAllByProductNumberIn(stockProductNumbers);
         return stocks.stream()
-                .collect(Collectors.toMap(Stock::getProductNumber, s -> s));
+            .collect(Collectors.toMap(Stock::getProductNumber, s -> s));
     }
 
     private Map<String, Long> createCountingMapBy(List<String> stockProductNumbers) {
         return stockProductNumbers.stream()
-                .collect(Collectors.groupingBy(p -> p, Collectors.counting()));
+            .collect(Collectors.groupingBy(p -> p, Collectors.counting()));
     }
 
     private List<Product> findProductsBy(List<String> productNumbers) {
         List<Product> products = productRepository.findAllByProductNumberIn(productNumbers);
         Map<String, Product> productMap = products.stream()
-                .collect(Collectors.toMap(Product::getProductNumber, p -> p));
+            .collect(Collectors.toMap(Product::getProductNumber, p -> p));
 
         return productNumbers.stream()
-                .map(productMap::get)
-                .collect(Collectors.toList());
+            .map(productMap::get)
+            .collect(Collectors.toList());
     }
 }
